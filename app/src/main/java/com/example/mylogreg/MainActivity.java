b3aec73102f56d50ce6db9e12dbbcea84e9628f9
Paarth <paarthpalande@gmail.com>
2020-03-02 08:07:11 +0530

    added database and connected project to database successfully

--- app/src/main/java/com/example/mylogreg/MainActivity.java
+++ app/src/main/java/com/example/mylogreg/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mylogreg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,28 +9,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username,password;
     Button Login;
     TextView signup;
+    FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth=FirebaseAuth.getInstance();
+
         username = (EditText)findViewById(R.id.uname);
         password = (EditText)findViewById(R.id.upass);
         Login = (Button)findViewById(R.id.login);
         signup = (TextView)findViewById(R.id.signup);
+
 
         Login.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                String uname=username.getText().toString().trim();
+                String upass=password.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(uname,upass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(),Users.class));
+                            Toast.makeText(getApplicationContext(),"Successfull..",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Failed..",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
 
