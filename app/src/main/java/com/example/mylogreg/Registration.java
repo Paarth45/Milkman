603d678836c53bc90e5754c7597fcb35b4dbcbad
Paarth <paarthpalande@gmail.com>
2020-03-02 08:09:30 +0530

    added database and connected project to database successfully

--- app/src/main/java/com/example/mylogreg/Registration.java
+++ app/src/main/java/com/example/mylogreg/Registration.java
@@ -1,24 +1,38 @@
 package com.example.mylogreg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Registration extends AppCompatActivity {
     EditText username,password,email,phone;
     Button signup;
     TextView signin;
+    FirebaseAuth firebaseAuth;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        firebaseAuth=FirebaseAuth.getInstance();
 
         username = (EditText) findViewById(R.id.uname1);
         email = (EditText)findViewById(R.id.uemail1);
@@ -32,6 +46,34 @@ public class Registration extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                String uname1=username.getText().toString().trim();
+                String uemail1=email.getText().toString().trim();
+                String upass1=password.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(uemail1)){
+                    email.setError("Recquired Field...");
+                    return;
+                }
+                if (TextUtils.isEmpty(upass1)){
+                    password.setError("Recquired Field");
+                    return;
+
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(uemail1,upass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(),Users.class));
+                            Toast.makeText(getApplicationContext(),"Successfull",Toast.LENGTH_SHORT).show();
+
+                        }else {
+                            Toast.makeText(getApplicationContext(),"Failed",Toast.LENGTH_SHORT).show();                        }
+                    }
+                });
+
+
 
             }
         });
